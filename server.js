f5a31fe78a8c059ad06614b5021c35c5464c093a
aaronpelzer <aaron.m.pelzer@gmail.com>
2017-03-20 20:06:18 +0000

    First

---
+++ server.js
@@ -0,0 +1,10 @@
+
+var express = require('express');
+
+
+
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
+});
